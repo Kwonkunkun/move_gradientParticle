c9687b2d5fec9f78f38989b9f22812c91be7c53a
Kwonkunkun <kunwoo5012@naver.com>
2021-06-07 18:59:20 +0900

    🔨 done

---
+++ app.js
@@ -0,0 +1,76 @@
+import { GlowParticle } from "./glowparticle.js";
+
+const COLORS = [
+  { r: 9, g: 132, b: 227 },
+  { r: 116, g: 185, b: 255 },
+  { r: 0, g: 206, b: 20 },
+  { r: 129, g: 236, b: 236 },
+  { r: 0, g: 184, b: 148 },
+  { r: 85, g: 239, b: 196 },
+];
+
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    document.body.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext("2d");
+
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    this.totalParticles = 20;
+    this.particles = [];
+    this.maxRadius = 900;
+    this.minRadius = 400;
+
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+    this.createParticles();
+  }
+
+  createParticles() {
+    let curColor = 0;
+    this.particles = [];
+
+    for (let i = 0; i < this.totalParticles; i++) {
+      const item = new GlowParticle(
+        Math.random() * this.stageWidth,
+        Math.random() * this.stageHeight,
+        Math.random() * (this.maxRadius - this.minRadius) + this.minRadius,
+        COLORS[curColor]
+      );
+
+      if (++curColor >= COLORS.length) {
+        curColor = 0;
+      }
+
+      this.particles[i] = item;
+    }
+  }
+
+  animate() {
+    window.requestAnimationFrame(this.animate.bind(this));
+
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+    for (let i = 0; i < this.totalParticles; i++) {
+      const item = this.particles[i];
+      item.animate(this.ctx, this.stageWidth, this.stageHeight);
+    }
+  }
+}
+
+window.onload = () => {
+  new App();
+};
